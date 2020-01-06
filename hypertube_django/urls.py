2112b390c1bd765d6e837ef29e6ce43c91b9a7a3
Ulmer Varly <uvarly@vo-c2.21-school.ru>
2020-01-06 16:42:41 +0300

    implemented auth module v0.1

--- hypertube_django/urls.py
+++ hypertube_django/urls.py
@@ -1,7 +1,7 @@
 """hypertube_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,22 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.urls import include, path
-from rest_framework import routers
-from temp import views
-
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-api_urlpatterns = [
-    path('accounts/', include('rest_registration.api.urls')),
-]
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api/v0.1/', include(api_urlpatterns)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest-framework'))
+    path('admin/', admin.site.urls),
+    path('api/v0.1/', include('rest_auth.urls'))
 ]
